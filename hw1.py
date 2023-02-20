6187c8eacd7eb92555fe1ff7c51438e19b73f949
alien588 <aljaz.zakosek@gmail.com>
2023-02-20 21:56:53 +0100

    Dodana kalkulacija razdalj s Mannhatansko in Evklidsko normo.
    TODO:
    - linkage
    - closest_clusters

--- hw1.py
+++ hw1.py
@@ -1,9 +1,65 @@
 def manhattan_dist(r1, r2):
     """ Arguments r1 and r2 are lists of numbers """
+    cum_sum = 0
+    cnt = 0
+    corrupt_cnt = 0
+    if len(r1) == len(r2):
+        for i in range(len(r1)):
+            if (r1[i] == r1[i]) and (r2[i] == r2[i]):
+                cum_sum += ((r1[i] - r2[i])**2)**(1/2)
+                cnt += 1
+            else:
+                corrupt_cnt += 1
+        if cnt == 0:
+            return float("nan")
+
+        if corrupt_cnt > 0:
+            r = cum_sum / cnt
+            for i in range(corrupt_cnt):
+                cum_sum += r
+        #cum_sum = cum_sum ** (1 / 2)
+        return cum_sum
     raise NotImplementedError()
 
 
 def euclidean_dist(r1, r2):
+    # r1 in r2 sta stevilcna vektorja, ki lahko vsebujeta NAN vrednosti.
+    # To resimo s normalizacijo koncne pridobljene sume.
+    # V kolikor si vektorja nista enako dolga, to resimo na taksen nacin, da vrednosti, ki bi jih morali primerjati na
+    # neenakih mestih pretiramo enako kot primerjave s NAN vnosi.
+    cum_sum = 0
+    cnt = 0
+    corrupt_cnt = 0
+    if len(r1) == len(r2):
+        for i in range(len(r1)):
+            if (r1[i] == r1[i]) and (r2[i] == r2[i]):
+                cum_sum += (r1[i] - r2[i])**2
+                cnt += 1
+            else:
+                corrupt_cnt += 1
+        if cnt == 0:
+            return float("nan")
+
+        if corrupt_cnt > 0:
+            r = cum_sum/cnt
+            for i in range(corrupt_cnt):
+                cum_sum += r
+        cum_sum = cum_sum ** (1/2)
+        return cum_sum
+    elif len(r1) < len(r2):
+        for i in range(len(r1)):
+            if (r1[i] == r1[i]) and (r2[i] == r2[i]):
+                cum_sum += (r1[i] - r2[i])**2
+                cnt += 1
+        cum_sum = cum_sum ** (1/2)
+        return cum_sum
+    else:
+        for i in range(len(r1)):
+            if (r1[i] == r1[i]) and (r2[i] == r2[i]):
+                cum_sum += (r1[i] - r2[i])**2
+                cnt += 1
+        cum_sum = cum_sum ** (1/2)
+        return cum_sum
     raise NotImplementedError()
 
 
@@ -12,6 +68,7 @@ def single_linkage(c1, c2, distance_fn):
     (lists of input vectors or rows).
     Argument distance_fn is a function that can compute
     a distance between two vectors (like manhattan_dist)."""
+    
     raise NotImplementedError()
 
 
